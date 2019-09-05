b6cd2a7a439e5630ae3f511c988da5b926d10c52
davisc0801 <davisc1014@gmail.com>
2019-09-05 17:21:03 -0600

    Change API test to a channel test
    Co-Authored-By: null <Patrick-Duvall@users.noreply.github.com>

---
+++ spec/channels/game_state_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "Game State Channel" do
+  setup do
+    @game = create(:game)
+    player = create(:player, game_id: @game.id)
+    gold = create_list(:gold, 30)
+    estates = create_list(:estate, 8)
+    @game.cards += gold
+    @game.cards += estates
+    stub_connection player: player
+  end
+
+  it "sends a successful response" do
+    subscribe
+    expect(subscription).to be_confirmed
+  end
+end
