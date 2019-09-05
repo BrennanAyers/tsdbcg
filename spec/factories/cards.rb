aa402ee78ff6d82b9c417eaa96251689f7e66702
davisc0801 <davisc1014@gmail.com>
2019-09-05 16:45:17 -0600

    Crate card factories
    Co-Authored-By: null <Patrick-Duvall@users.noreply.github.com>

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,9 +1,48 @@
 FactoryBot.define do
   factory :card do
-    name { "MyString" }
-    type { "" }
-    cost { 1 }
-    victoryPoints { 1 }
+
+  end
+
+  factory :gold, parent: :card do
+    name { "Gold" }
+    type { "Money" }
+    cost { 6 }
+    victoryPoints { nil }
+    spendingPower { 3 }
+  end
+  factory :silver, parent: :card do
+    name { "Silver" }
+    type { "Money" }
+    cost { 3 }
+    victoryPoints { nil }
+    spendingPower { 2 }
+  end
+  factory :copper, parent: :card do
+    name { "Copper" }
+    type { "Money" }
+    cost { 0 }
+    victoryPoints { nil }
     spendingPower { 1 }
+  end
+  factory :estate, parent: :card do
+    name { "Estate" }
+    type { "Victory" }
+    cost { 2 }
+    victoryPoints { 1 }
+    spendingPower { 0 }
+  end
+  factory :duchy, parent: :card do
+    name { "Duchy" }
+    type { "Victory" }
+    cost { 5 }
+    victoryPoints { 3 }
+    spendingPower { 0 }
+  end
+  factory :province, parent: :card do
+    name { "Province" }
+    type { "Victory" }
+    cost { 8 }
+    victoryPoints { 6 }
+    spendingPower { 0 }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require './support/factorybot'
+require_relative './support/factorybot'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
