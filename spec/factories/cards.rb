edd17aca70fccd85e5c32289f55c2f00999db205
davisc0801 <davisc1014@gmail.com>
2019-09-05 16:09:25 -0600

    Add factory defaults for all models
    Co-Authored-By: null <Patrick-Duvall@users.noreply.github.com>

---
+++ spec/factories/cards.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :card do
+    name { "MyString" }
+    type { "" }
+    cost { 1 }
+    victoryPoints { 1 }
+    spendingPower { 1 }
+  end
+end
---
+++ spec/factories/players.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :player do
+    name { "MyString" }
+    deck { nil }
+    game { nil }
+  end
+end
