b3601f75bb013577ba4936998a663f6afb70bdf0
davisc0801 <davisc1014@gmail.com>
2019-09-05 16:08:57 -0600

    Create and run migrations for database schema
    Co-Authored-By: null <Patrick-Duvall@users.noreply.github.com>

---
+++ db/migrate/2_create_cards.rb
@@ -0,0 +1,13 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.string :type
+      t.integer :cost
+      t.integer :victoryPoints
+      t.integer :spendingPower
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,69 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 6) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "cost"
+    t.integer "victoryPoints"
+    t.integer "spendingPower"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.bigint "player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_decks_on_player_id"
+  end
+
+  create_table "game_cards", force: :cascade do |t|
+    t.bigint "game_id"
+    t.bigint "card_id"
+    t.bigint "deck_id"
+    t.boolean "trashed"
+    t.boolean "discarded"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_id"], name: "index_game_cards_on_card_id"
+    t.index ["deck_id"], name: "index_game_cards_on_deck_id"
+    t.index ["game_id"], name: "index_game_cards_on_game_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.bigint "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "deck_id"
+    t.index ["deck_id"], name: "index_players_on_deck_id"
+    t.index ["game_id"], name: "index_players_on_game_id"
+  end
+
+  add_foreign_key "decks", "players"
+  add_foreign_key "game_cards", "cards"
+  add_foreign_key "game_cards", "decks"
+  add_foreign_key "game_cards", "games"
+  add_foreign_key "players", "decks"
+  add_foreign_key "players", "games"
+end
