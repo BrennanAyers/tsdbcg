1458bbb9f2fea4deebe313761829f64af437a059
davisc0801 <davisc1014@gmail.com>
2019-09-05 16:09:25 -0600

    Add factory defaults for all models
    Co-Authored-By: null <Patrick-Duvall@users.noreply.github.com>

---
+++ spec/factories/decks.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :deck do
+    player { nil }
+  end
+end
---
+++ spec/factories/game_cards.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :game_card do
+    game_id { nil }
+    card_id { nil }
+    deck_id { nil }
+    trashed { false }
+    discarded { false }
+  end
+end
