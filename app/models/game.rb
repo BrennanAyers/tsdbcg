0cf4c392299489088f388d14bab52c1558bd1490
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-07 15:12:55 -0600

    Add initial #start method for Game that adds Copper and Estates to Player GameCards, and adds Coppers and Estates to pool of GameCards
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,23 @@ class Game < ApplicationRecord
 has_many :players
 has_many :game_cards
 has_many :cards, through: :game_cards
+
+  def start
+    copper = Card.find_by(name: "Copper")
+    estate = Card.find_by(name: "Estate")
+    players.each do |player|
+      7.times do
+        GameCard.create(card_id: copper.id, game_id: id, player_id: player.id)
+      end
+      3.times do
+        GameCard.create(card_id: estate.id, game_id: id, player_id: player.id)
+      end
+    end
+    60.times do
+      GameCard.create(card_id: copper.id, game_id: id)
+    end
+    8.times do
+      GameCard.create(card_id: estate.id, game_id: id)
+    end
+  end
 end
