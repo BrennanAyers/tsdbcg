ab1c8ff4841117c47c62d56720b8377f672b8efa
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-07 17:18:36 -0600

    Add additonal key checks on GameCards for Player State endpoint spec
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- spec/requests/player_state_spec.rb
+++ spec/requests/player_state_spec.rb
@@ -16,8 +16,23 @@ describe 'Player State API' do
 
     data = JSON.parse(response.body)
 
-    expect(data["playerId"]).to eq(@player.id)
-    expect((data["deck"]).count).to eq(10)
-    expect((data["discard"]).count).to eq(0)
+    expect(data['playerId']).to eq(@player.id)
+    expect((data['deck']).count).to eq(10)
+    expect((data['discard']).count).to eq(0)
+
+    expect(data['deck'].first).to have_key('name')
+    expect(data['deck'].first).to have_key('category')
+    expect(data['deck'].first).to have_key('cost')
+    expect(data['deck'].first).to have_key('spendingPower')
+    expect(data['deck'].first).to have_key('victoryPoints')
+    expect(data['deck'].first).to have_key('image')
+    expect(data['deck'].first).to have_key('id')
+    expect(data['deck'].last).to have_key('name')
+    expect(data['deck'].last).to have_key('category')
+    expect(data['deck'].last).to have_key('cost')
+    expect(data['deck'].last).to have_key('spendingPower')
+    expect(data['deck'].last).to have_key('victoryPoints')
+    expect(data['deck'].last).to have_key('image')
+    expect(data['deck'].last).to have_key('id')
   end
 end
