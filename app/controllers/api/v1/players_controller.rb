1d25d25a8e795fe22eeb22e2472e427c0b6e25cb
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-07 18:04:48 -0600

    Move Player serialization from PlayersController to PlayerSerializer

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,26 +1,6 @@
 class Api::V1::PlayersController < ApplicationController
   def show
     player = Player.find(params[:player_id])
-    render json: {
-      playerId: player.id,
-      deck: player.deck.map {|card| {
-        name: card.card.name,
-        category: card.card.category.split(','),
-        cost: card.card.cost,
-        spendingPower: card.card.spending_power,
-        victoryPoints: card.card.victory_points,
-        image: "#{card.card.name}.img",
-        id: card.id
-        }},
-      discard: player.discard.map {|card| {
-        name: card.card.name,
-        category: card.card.category.split(','),
-        cost: card.card.cost,
-        spendingPower: card.card.spending_power,
-        victoryPoints: card.card.victory_points,
-        image: "#{card.card.name}.img",
-        id: card.id
-        }}
-      }
+    render json: PlayerSerializer.new(player).cards
   end
 end
