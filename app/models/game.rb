8038a0e151745d6d1d1a01222647c78dbfbd487b
davisc0801 <davisc1014@gmail.com>
2019-09-09 14:38:54 -0600

    Add instance methods to player and game class to get info about the current game state

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,10 @@
 class Game < ApplicationRecord
-has_many :players
-has_many :game_cards
-has_many :cards, through: :game_cards
+  has_many :players
+  has_many :game_cards
+  has_many :cards, through: :game_cards
+
+  def player_order
+    #todo - update to track player order
+    return players
+  end
 end
