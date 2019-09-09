2849b7d0df9d32429dd38a78ff8bda27ca4a14c2
davisc0801 <davisc1014@gmail.com>
2019-09-09 14:38:06 -0600

    Add player information to the game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -3,16 +3,31 @@ class GameSerializer
     @game = game
   end
 
-  def cards
-    @game.cards.distinct.select("id","name","category","cost","victory_points","spending_power").map do |card|
+  def state
+    card_arr = @game.cards.distinct.select("id","name","category","cost","victory_points","spending_power").map do |card|
       {
         name: card.name,
         category: card.category,
         cost: card.cost,
         victoryPoints: card.victory_points,
         spendingPower: card.spending_power,
-        countAvailable: @game.game_cards.where(card_id: card.id).count
+        countAvailable: @game.game_cards.where(card_id: card.id, player_id: nil).count
       }
     end
+    players = @game.player_order
+    player_names = players.map {|player| player.name}
+    player_info_hash = {}
+    players.each do |player|
+      player_info_hash[player.name] = {
+        deckSize: player.cards.length,
+        topCardDiscard: player.fetch_top_discard,
+        handSize: player.fetch_hand_size
+      }
+    end
+    return {
+      game_cards: card_arr,
+      player_order: player_names,
+      player_info: player_info_hash
+    }
   end
 end
