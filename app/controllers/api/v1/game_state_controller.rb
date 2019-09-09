6bd756295519e504ce9d88fa361a92006c05dbe0
davisc0801 <davisc1014@gmail.com>
2019-09-09 14:38:29 -0600

    Change method call to state instead of cards in controller

--- app/controllers/api/v1/game_state_controller.rb
+++ app/controllers/api/v1/game_state_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::GameStateController < ApplicationController
   def index
     game = Game.find(params[:id])
     serialized_game = GameSerializer.new(game)
-    render json: serialized_game.cards
+    render json: serialized_game.state
   end
 end
