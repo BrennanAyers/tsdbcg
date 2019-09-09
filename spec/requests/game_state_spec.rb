ffccbf82ee65f04c38f4edbe5d299f4924e51e60
davisc0801 <davisc1014@gmail.com>
2019-09-09 17:34:48 -0600

    Add spec to reflect additional card info

--- spec/requests/game_state_spec.rb
+++ spec/requests/game_state_spec.rb
@@ -6,48 +6,45 @@ describe 'Game State API' do
     gold = create(:gold)
     copper = create(:copper)
     estate = create(:estate)
-    player = create(:player, game_id: @game.id)
-    create_list(:game_card, 3, game_id: @game.id, card_id: estate.id, player_id: player.id)
-    create_list(:game_card, 7, game_id: @game.id, card_id: copper.id, player_id: player.id)
+    @player = create(:player, game_id: @game.id)
+    create_list(:game_card, 3, game_id: @game.id, card_id: estate.id, player_id: @player.id)
+    create_list(:game_card, 7, game_id: @game.id, card_id: copper.id, player_id: @player.id)
     create_list(:game_card, 30, game_id: @game.id, card_id: gold.id)
     create_list(:game_card, 8, game_id: @game.id, card_id: estate.id)
   end
 
   it 'sends the initial game state' do
     get "/api/v1/game_state/#{@game.id}"
-    # require 'pry'; binding.pry
-    expect(JSON.parse(response.body)).to eq("game_cards" =>
-    [{
-      "name" => "Gold",
-      "category" => "Money",
-      "cost" => 6,
-      "victoryPoints" => nil,
-      "spendingPower" => 3,
-      "countAvailable" => 30
-    },
-    {
-      "name" => "Copper",
-      "category" => "Money",
-      "cost" => 0,
-      "victoryPoints" => nil,
-      "spendingPower" => 1,
-      "countAvailable" => 0
-    },
-    {
-      "name" => "Estate",
-      "category" => "Victory",
-      "cost" => 2,
-      "victoryPoints" => 1,
-      "spendingPower" => nil,
-      "countAvailable" => 8
-    }],
-    "player_order" => ["MyString"],
-    "player_info" => {"MyString" =>
-      { "deckSize" => 10,
-        "topCardDiscard" => nil,
-        "handSize" => 5
-      }
-    }
-  )
+    expect(JSON.parse(response.body)).to have_key('tableDeck')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('name')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('category')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('victoryPoints')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('spendingPower')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('buyingPower')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('actionsProvided')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('cardsToDraw')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('image')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('desc')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('tags')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('countAvailable')
+    expect(JSON.parse(response.body)["tableDeck"].first).to have_key('id_list')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('name')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('category')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('victoryPoints')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('spendingPower')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('buyingPower')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('actionsProvided')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('cardsToDraw')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('image')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('desc')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('tags')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('countAvailable')
+    expect(JSON.parse(response.body)["tableDeck"].last).to have_key('id_list')
+    expect(JSON.parse(response.body)).to have_key('playerOrder')
+    expect(JSON.parse(response.body)).to have_key('playerInfo')
+    expect(JSON.parse(response.body)["playerInfo"]).to have_key(@player.name)
+    expect(JSON.parse(response.body)["playerInfo"][@player.name]).to have_key("deckSize")
+    expect(JSON.parse(response.body)["playerInfo"][@player.name]).to have_key("topCardDiscard")
+    expect(JSON.parse(response.body)["playerInfo"][@player.name]).to have_key("handSize")
   end
 end
