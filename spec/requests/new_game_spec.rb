f306ef1d3d7a96198816b017ddd0ff4d77af825d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-10 10:14:08 -0600

    Add initial New Game endpoint spec

---
+++ spec/requests/new_game_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'New Game API' do
+  before :each do
+
+  end
+
+  it 'sends a player name and receives a newly generated Game' do
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+    json_payload = {
+      player_name: "Test"
+      }
+    post "/api/v1/games", headers: headers, params: json_payload.to_json
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+
+    expect(data["player_name"]).to eq("Test")
+    expect(data["player_id"]).to_not be nil
+    expect(data["game_id"]).to_not be nil
+
+    expect(Game.last.players.first.name).to eq("Test")
+  end
+end
