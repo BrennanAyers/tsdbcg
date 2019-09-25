a38bf4f50623fb12dcfa2d5a8f69e9cb11836aa9
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-25 14:17:10 -0600

    Add expectations to GameStateChannel when Player joins for message data with type, id, and name of Player
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- spec/channels/game_state_channel_spec.rb
+++ spec/channels/game_state_channel_spec.rb
@@ -18,5 +18,12 @@ describe GameStateChannel, type: :channel do
   it 'broadcasts player info when one player subscribes' do
     expect{ subscribe }.to have_broadcasted_to(game)
       .from_channel(GameStateChannel)
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq('player-joined')
+        payload = message[:data]
+        expect(payload[:id]).to eq(@player.id)
+        expect(payload[:name]).to eq(@player.name)
+      }
   end
 end
