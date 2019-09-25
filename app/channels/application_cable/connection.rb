95ace1c5516a503c3b5a407b2e3786ce8e0eb5f7
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-25 14:59:48 -0600

    Add initial ActionCable Connection identified by current_player, Add connect method with find_verified_player private method to find and assign current_player by player_id

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      self.current_player = find_verified_player
+    end
+
+    private
+
+    def find_verified_player
+      if verified_player = Player.find(request.params[:player_id])
+        verified_player
+      end
+    end
   end
 end
