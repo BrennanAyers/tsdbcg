ba5000fe07e6bb730b25cd89a13d96b2083edf8d
Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
2019-09-26 10:47:56 -0600

    Refactor GameStateChannel with private broadcast_player_joined method called when a new Player joins the Game
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- app/channels/game_state_channel.rb
+++ app/channels/game_state_channel.rb
@@ -1,8 +1,18 @@
 class GameStateChannel < ApplicationCable::Channel
+  on_subscribe :broadcast_player_joined
+
   def subscribed
     ensure_confirmation_sent
     stream_for current_player.game
+  end
 
+  private
+
+  def broadcast_message(payload)
+    GameStateChannel.broadcast_to current_player.game, message: payload.to_json
+  end
+
+  def broadcast_player_joined
     payload = {
       type: 'player-joined',
       data: {
@@ -12,12 +22,6 @@ class GameStateChannel < ApplicationCable::Channel
       }
     }
     broadcast_message payload
-  end
-
-  private
-
-  def broadcast_message(payload)
-    GameStateChannel.broadcast_to current_player.game, message: payload.to_json
   end
 
   def player_list_generator(game)
