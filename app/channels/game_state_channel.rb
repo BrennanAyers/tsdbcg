52f65f7c4d2b42306e1f24e2bb2501d08ad53fa6
Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
2019-09-26 10:44:26 -0600

    Add playerList information to player-joined message on GameStateChannel, Add private player_list_generator method on GameStateChannel to serialize all currently joined Players in current_player.game
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- app/channels/game_state_channel.rb
+++ app/channels/game_state_channel.rb
@@ -7,7 +7,8 @@ class GameStateChannel < ApplicationCable::Channel
       type: 'player-joined',
       data: {
         id: current_player.id,
-        name: current_player.name
+        name: current_player.name,
+        playerList: player_list_generator(current_player.game)
       }
     }
     broadcast_message payload
@@ -17,5 +18,14 @@ class GameStateChannel < ApplicationCable::Channel
 
   def broadcast_message(payload)
     GameStateChannel.broadcast_to current_player.game, message: payload.to_json
+  end
+
+  def player_list_generator(game)
+    game.players.map do |player|
+      {
+        id: player.id,
+        name: player.name
+      }
+    end
   end
 end
