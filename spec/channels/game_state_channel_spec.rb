3d3e7ba8567e19321c6dc486d9799a9cb54948fb
Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
2019-10-01 15:26:49 -0600

    Add spec for starting a Game when enough Players subscribe to the Game with 4 Players and Broadcasting GameState
    Co-Authored-By: Patrick Duvall <duvallp208@gmail.com>

--- spec/channels/game_state_channel_spec.rb
+++ spec/channels/game_state_channel_spec.rb
@@ -52,4 +52,90 @@ describe GameStateChannel, type: :channel do
         expect(player_ids).to include(second_player.id)
       }
   end
+
+  it 'broadcasts game information when enough players join and game starts' do
+    create_cards
+
+    subscribe
+
+    second_player = game.players.create(name: 'Johnson Johnsonson')
+    stub_connection current_player: second_player
+
+    subscribe
+
+    third_player = game.players.create(name: 'Don Donsonson')
+    stub_connection current_player: third_player
+
+    subscribe
+
+    fourth_player = game.players.create(name: 'Sean Seansonson')
+    stub_connection current_player: fourth_player
+
+    expect{ subscribe }.to have_broadcasted_to(game)
+      .from_channel(GameStateChannel)
+      .twice
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        unless message[:type] == 'player-joined'
+          expect(message[:type]).to eq('game-started')
+          payload = message[:data]
+          expect(payload).to have_key(:tableDeck)
+          expect(payload[:tableDeck].first).to have_key(:name)
+          expect(payload[:tableDeck].first).to have_key(:category)
+          expect(payload[:tableDeck].first).to have_key(:victoryPoints)
+          expect(payload[:tableDeck].first).to have_key(:spendingPower)
+          expect(payload[:tableDeck].first).to have_key(:buyingPower)
+          expect(payload[:tableDeck].first).to have_key(:actionsProvided)
+          expect(payload[:tableDeck].first).to have_key(:cardsToDraw)
+          expect(payload[:tableDeck].first).to have_key(:image)
+          expect(payload[:tableDeck].first).to have_key(:desc)
+          expect(payload[:tableDeck].first).to have_key(:tags)
+          expect(payload[:tableDeck].first).to have_key(:countAvailable)
+          expect(payload[:tableDeck].first).to have_key(:id_list)
+          expect(payload[:tableDeck].last).to have_key(:name)
+          expect(payload[:tableDeck].last).to have_key(:category)
+          expect(payload[:tableDeck].last).to have_key(:victoryPoints)
+          expect(payload[:tableDeck].last).to have_key(:spendingPower)
+          expect(payload[:tableDeck].last).to have_key(:buyingPower)
+          expect(payload[:tableDeck].last).to have_key(:actionsProvided)
+          expect(payload[:tableDeck].last).to have_key(:cardsToDraw)
+          expect(payload[:tableDeck].last).to have_key(:image)
+          expect(payload[:tableDeck].last).to have_key(:desc)
+          expect(payload[:tableDeck].last).to have_key(:tags)
+          expect(payload[:tableDeck].last).to have_key(:countAvailable)
+          expect(payload[:tableDeck].last).to have_key(:id_list)
+          expect(payload).to have_key(:playerOrder)
+          expect(payload).to have_key(:playerInfo)
+          expect(payload[:playerInfo]).to have_key(@player.name.to_sym)
+          expect(payload[:playerInfo][@player.name.to_sym]).to have_key(:deckSize)
+          expect(payload[:playerInfo][@player.name.to_sym]).to have_key(:topCardDiscard)
+          expect(payload[:playerInfo][@player.name.to_sym]).to have_key(:handSize)
+          expect(payload[:activePlayerName]).to eq(@player.name)
+          expect(payload[:activePlayerId]).to eq(@player.id)
+        else
+          expect(message[:type]).to eq('player-joined')
+        end
+      }
+  end
+end
+
+def create_cards
+  image_url = "http://127.0.0.1:3000/card_images/"
+  Card.create(name: "Copper", category: "Money", cost: 0, spending_power: 1, tags: "", desc: "", image: image_url + "Copper.jpg")
+  Card.create(name: "Silver", category: "Money", cost: 3, spending_power: 2, tags: "", desc: "", image: image_url + "Silver.jpg")
+  Card.create(name: "Gold", category: "Money", cost: 6, spending_power: 3, tags: "", desc: "", image: image_url + "Gold.jpg")
+  Card.create(name: "Estate", category: "Victory", cost: 2, victory_points: 1, tags: "", desc: "", image: image_url + "Estate.jpg")
+  Card.create(name: "Duchy", category: "Victory", cost: 5, victory_points: 3, tags: "", desc: "", image: image_url + "Duchy.jpg")
+  Card.create(name: "Province", category: "Victory", cost: 8, victory_points: 6, tags: "", desc: "", image: image_url + "Province.jpg")
+
+  Card.create(name: "Village", category: "Action", cost: 3, actions_provided: 2, cards_to_draw: 1, tags: "+1 Card,+2 Actions", desc: "", image: image_url + "Village.jpg")
+  Card.create(name: "Militia", category: "Action,Attack", cost: 4, spending_power: 2, tags: "+2 Gold", desc: "Each other play discards down to 3 cards in their hand.", image: image_url + "Militia.jpg")
+  Card.create(name: "Smithy", category: "Action", cost: 4, cards_to_draw: 3, tags: "+3 Cards", desc: "", image: image_url + "Smithy.jpg")
+  Card.create(name: "Market", category: "Action", cost: 5, actions_provided: 1, cards_to_draw: 1, spending_power: 1, buying_power: 1, tags: "+1 Card,+1 Action,+1 Buy,+1 Gold", desc: "", image: image_url + "Market.jpg")
+  Card.create(name: "Mine", category: "Action", cost: 5, tags: "", desc: "Trash a Treasure card from your hand. Gain a Treasure card costing up to 3 Treasure more; put it in your hand.", image: image_url + "Mine.jpg")
+  Card.create(name: "Remodel", category: "Action", cost: 4, tags: "", desc: "Trash a card from your hand. Gain a card costing up to 2 treasure more than the trashed card.", image: image_url + "Remodel.jpg")
+  Card.create(name: "Cellar", category: "Action", cost: 2, actions_provided: 1, tags: "+1 Action", desc: "Discard any number of cards. +1 Card per card discarded.", image: image_url + "Cellar.jpg")
+  Card.create(name: "Moat", category: "Action,Reaction", cost: 2, cards_to_draw: 2, tags: "+2 Cards", desc: "When another player plays an Attack card, you maye reveal this from your hand. If you do, you are unaffected by that attack.", image: image_url + "Moat.jpg")
+  Card.create(name: "Woodcutter", category: "Action", cost: 3, tags: "+1 Buy,+2 Gold", spending_power: 2, buying_power: 1, desc: "", image: image_url + "Woodcutter.jpg")
+  Card.create(name: "Workshop", category: "Action", cost: 3, tags: "", desc: "Gain a card costing up to 4 treasure.", image: image_url + "Workshop.jpg")
 end
